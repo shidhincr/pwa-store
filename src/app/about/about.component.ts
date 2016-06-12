65ffcdfe454b74bbb13f98b21c2ac604c80410a8
Shidhin C R <shidhincr@gmail.com>
2016-06-12 14:50:32 +0400

    some UI fixes

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -6,6 +6,16 @@ import {MD_CARD_DIRECTIVES} from "@angular2-material/card/card";
   styles: [`
     .about {
       margin: 10px;
+      transform: scale(1, 1);
+      animation: scale 500ms;
+    }
+    @keyframes scale {
+      from {
+        transform: scale(0.1, 0.1);
+      }
+      to {
+        transform: scale(1, 1);
+      }
     }
   `],
   template: `
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
-import { enableProdMode } from '@angular/core';
-import { bootstrap } from '@angular/platform-browser-dynamic';
-import { ROUTER_PROVIDERS } from '@angular/router-deprecated';
-import { HTTP_PROVIDERS } from '@angular/http';
+import {enableProdMode} from '@angular/core';
+import {bootstrap} from '@angular/platform-browser-dynamic';
+import {ROUTER_PROVIDERS} from '@angular/router-deprecated';
+import {HTTP_PROVIDERS} from '@angular/http';
 
-import { AppComponent } from './app/app.component';
+import {AppComponent} from './app/app.component';
 
 if ('prod' === ENV) {
   enableProdMode();
